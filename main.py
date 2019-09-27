6e523f42694d41dacf6e37d08cdf898b462ef7d7
A Bokov <lively.bokov@gmail.com>
2019-09-26 21:28:42 -0500

    made X it things that you have done

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ PREFIX = 'hangman'
 HANGMAN = [xx.replace('.png', '') for xx in os.listdir('images') if xx.startswith('hang')]
 HANGMAN.sort()
 HHEIGHT = Actor(HANGMAN[0]).height
+HWIDTH = Actor(HANGMAN[0]).width
 hpix = 0
 winlose = None
 words = "./words/current_spelling_words.txt"
 spelling = open(words, "r").read().splitlines()[1:]
+spelling = list(set(spelling))
 current_word = random.choice(spelling)
 placeholder = ["_"] * len(current_word)
+guessedlett = []
+guessedw = []
 
 def on_key_down(key,mod,unicode):
-    global current_word, spelling, placeholder, hpix, winlose
+    global current_word, spelling, placeholder, hpix, winlose, guessedlett, guessedw
     if winlose:
         winlose = None
     elif unicode in current_word: # if you guessed right
@@ -23,22 +27,26 @@ def on_key_down(key,mod,unicode):
             # This triggers when you guessed the whole word
             hpix = 0
             spelling.remove(current_word)
+            guessedw += [current_word]
             if spelling:
                 current_word = random.choice(spelling)
                 placeholder = ["_"] * len(current_word)
+                guessedlett = []
                 winlose = 'victory'
             else:
                 print("w00t")
                 return()
     else: # if you guessed wrong...
         hpix += 1
+        guessedlett += [unicode]
         if hpix == len (HANGMAN):
-            print('GAME OVER!')
+            print('incorrect!')
             hpix = 0
             current_word = random.choice(spelling)
             placeholder = ["_"] * len(current_word)
+            guessedlett = []
             winlose = 'defeat'
-
+            
 def draw():
     screen.clear()
     if winlose:
@@ -47,6 +55,9 @@ def draw():
         screen.blit(HANGMAN[hpix], (1,1))
     #screen.draw.text(current_word, (20,HHEIGHT + 10))
     screen.draw.text(''.join(placeholder), (20,HHEIGHT + 30))
+    screen.draw.text(" ".join(guessedlett),(20,HHEIGHT + 45),
+                     color = (255,0,0))
+    screen.draw.text('\n'.join(guessedw), (HWIDTH + 40, 10))
     
 pgzrun.go()
 
